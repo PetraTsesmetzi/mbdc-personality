ed32cbb1883e086a2b921d931dda4e6b3aaab96a
Petra T <p_tsesmetzi@gmx.de>
2020-05-17 21:48:44 +0200

    animation stopper

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import "./css/general.css";
 import "./css/footer.css";
 
 document.addEventListener("DOMContentLoaded", () => {
+  // stop animation if device has been rotated
+  let resizeTimer;
+  window.addEventListener("resize", () => {
+    document.body.classList.add("resize_animation_stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove("resize_animation_stopper");
+    }, 400);
+  });
   new Index();
-  /* new NavBar(); */
 });
