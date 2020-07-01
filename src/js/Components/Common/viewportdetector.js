4942c33e005a9350d29b202188122fdd324e896e
Petra T <p_tsesmetzi@gmx.de>
2020-07-01 22:50:39 +0200

    comments to the viewportdetector

--- src/js/Components/Common/viewportdetector.js
+++ src/js/Components/Common/viewportdetector.js
@@ -1,14 +1,19 @@
+/* Viewportdetector-static class
+detects the size of the viewport
+*/
 export class Viewportdetector {
   constructor() {
     this.scroll = 0;
     this.start = 0;
   }
+  //sets the distance of the scroll depending on the screensize
   static detectDistance() {
     let vw = window.innerWidth;
 
     vw < 768 ? (this.scroll = 212) : (this.scroll = 155);
     return this.scroll;
   }
+  //ssets the startpoint of the scrollposition depending on the screensize
   static detectStartPosition() {
     let vw = window.innerWidth;
 
