10dd5e6e07c02f7d4bd4c110d01de391707bc930
Petra T <p_tsesmetzi@gmx.de>
2020-07-01 22:42:51 +0200

    comments to functions

--- src/js/Routing/actions.js
+++ src/js/Routing/actions.js
@@ -3,31 +3,35 @@ actions-mehtodes
 if there is an action- like a route was changed- the called fuction will set the
 the new attribute
 */
+//updates the route-object
 export function loadRoute(route) {
   return {
     type: "CHANGE_ROUTE",
     route,
   };
 }
+//updates the personaltyType-object
 export function sendType(personaltyType) {
   return {
     type: "CHANGE_TYPE",
     personaltyType,
   };
 }
+//uptdates testresult-object
 export function sendTestresult(testresult) {
   return {
     type: "CHANGE_TESTRESULT",
     testresult,
   };
 }
-
+//updates the number of the progressbar
 export function increaseProgress(number) {
   return {
     type: "INCREASE_PROGRESS",
     number,
   };
 }
+//update(resets) the number of the progressbar
 export function resetProgress(number) {
   return {
     type: "RESET_PROGRESS",
