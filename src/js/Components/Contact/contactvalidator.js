a759417d64884db08910abdd3b05a4128d4e8512
Petra T <p_tsesmetzi@gmx.de>
2020-07-02 22:02:20 +0200

    add comments

--- src/js/Components/Contact/contactvalidator.js
+++ src/js/Components/Contact/contactvalidator.js
@@ -1,8 +1,15 @@
+/* 
+ContactFormValidator
+checks out if the form was correctly filled out
+*/
 export class ContactFormValidator {
   constructor() {
     this.form = document.getElementsByTagName("form");
     this.checkform(this.form[0]);
   }
+
+  //checking if the firstname lastname email and the comments are not blank
+  //also if the email is valide
   checkform(form) {
     const alltrue = [];
     [...form].forEach((input) => {
@@ -48,22 +55,26 @@ export class ContactFormValidator {
       form.reset();
     }
   }
+  //set the css classes if an error occurs
   setErrorFor(input, message) {
     const formcontrol = input.parentElement.parentElement;
     const errorContainer = formcontrol.querySelector(".error_message");
     errorContainer.innerText = message;
     formcontrol.className = "contact_row contactform-error";
   }
+  //removes the css classes if the input is filled correctly
   takeOffError(input) {
     const formcontrol = input.parentElement.parentElement;
     const errorContainer = formcontrol.querySelector(".error_message");
     errorContainer.innerText = "";
     formcontrol.className = "contact_row";
   }
+  //check if it is an email
   isEmail(mail) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(mail);
   }
+  //set css classes for dsplaying the errowmessages
   showFinalMessage() {
     var finalMessage = document.getElementsByClassName("sent_successfully");
     finalMessage[0].classList.add("successfuly_active");
