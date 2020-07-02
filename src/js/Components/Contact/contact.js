399eb182f255c23631e751ded0aaadd3698d7931
Petra T <p_tsesmetzi@gmx.de>
2020-07-02 21:50:30 +0200

    add comments

--- src/js/Components/Contact/contact.js
+++ src/js/Components/Contact/contact.js
@@ -1,8 +1,12 @@
 import "./../../../css/contact.css";
 import { Banner } from "../Common/banner";
 import { ContactForm } from "./contactform";
-
+/* 
+Contact
+displays a banner and a contactform
+*/
 export class Contact extends HTMLElement {
+  //creates a new banner  an a form object
   constructor() {
     super();
     const bannerconfig = {
@@ -16,12 +20,14 @@ export class Contact extends HTMLElement {
     this.banner = new Banner(bannerconfig);
     this.contactform = new ContactForm();
   }
+  // appends the banner and the form object
   connectedCallback() {
     this.render();
     let test_wrapper = document.getElementById("contactcontainer_wrapper");
     test_wrapper.prepend(this.banner);
     test_wrapper.append(this.contactform);
   }
+  //displays the banner an the form
   render() {
     this.innerHTML = `<section id="contactcontainer_wrapper"> </section>`;
   }
