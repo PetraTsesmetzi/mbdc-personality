3b331ee3111b81e5df6b03bbee001bcc3c34e268
Petra T <p_tsesmetzi@gmx.de>
2020-07-03 12:50:06 +0200

    comments to default/home

--- src/js/Components/default.js
+++ src/js/Components/default.js
@@ -2,17 +2,24 @@ import "./../../css/home.css";
 import { store } from "./Common/store";
 import image from "../../img/banner_index.png";
 import { loadRoute } from "../Routing/actions";
-
+/* 
+DefaultComponent
+the first site if you load the app, also the home site
+*/
 export class DefaultComponent extends HTMLElement {
+  //implements the stor and the path for the button
   constructor() {
     super();
     this.store = store;
     this.path = "personalitytest";
   }
-
+  //load the route for the personalitytest after pushing the button
   handleClick() {
     this.store.dispatch(loadRoute({ path: this.path }));
   }
+  //subscrbes to the store
+  //add an eventlister to the button
+  //adds a css "on" class..this sets the hight of the content on auto
   connectedCallback() {
     let content = document.getElementById("content");
     content.classList.add("on");
@@ -21,10 +28,12 @@ export class DefaultComponent extends HTMLElement {
     let button = document.getElementById("test_btn");
     button.addEventListener("click", this.handleClick.bind(this));
   }
+
   disconnectedCallback() {
     let content = document.getElementById("content");
     content.classList.remove("on");
   }
+  //displays the default/home page
   render() {
     this.innerHTML = `
    
